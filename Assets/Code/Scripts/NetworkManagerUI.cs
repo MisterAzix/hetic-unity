320ba4211112424ef6d82afe3e8cd9a09e30c97a
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2023-01-20 01:21:04 +0100

    feat: improve start GUI ✨ (#19)

--- Assets/Code/Scripts/NetworkManagerUI.cs
+++ Assets/Code/Scripts/NetworkManagerUI.cs
@@ -1,29 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NetworkManagerUI : MonoBehaviour
 {
-    [SerializeField] private Button serverBtn;
     [SerializeField] private Button hostBtn;
     [SerializeField] private Button clientBtn;
+    [SerializeField] private TMP_InputField serverInput;
+
+    [SerializeField] private string serverIP = "51.38.33.24";
+    [SerializeField] private int serverPORT = 4242;
 
     private void Awake()
     {
-       
-        serverBtn.onClick.AddListener(() =>
+        hostBtn.onClick.AddListener(() => Host());
+        clientBtn.onClick.AddListener(() => Client());
+    }
+
+    private void Host()
+    {
+        UnityTransport unityTransport = NetworkManager.Singleton.gameObject.GetComponent<UnityTransport>();
+        serverIP = "127.0.0.1";
+
+        unityTransport.SetConnectionData(serverIP, (ushort)serverPORT);
+        NetworkManager.Singleton.OnClientConnectedCallback += HandleClientConnected;
+        NetworkManager.Singleton.StartHost();
+    }
+
+    private void Client()
+    {
+        UnityTransport unityTransport = NetworkManager.Singleton.gameObject.GetComponent<UnityTransport>();
+
+        if (serverInput.text.Length >= 1)
         {
-            NetworkManager.Singleton.StartServer();
-        });
-        hostBtn.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.StartHost();
-        });
-        clientBtn.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.StartClient();
-        });
+            serverIP = serverInput.text;
+        }
+
+        Debug.Log(serverIP);
+
+        unityTransport.SetConnectionData(serverIP, (ushort)serverPORT);
+        NetworkManager.Singleton.OnClientConnectedCallback += HandleClientConnected;
+        NetworkManager.Singleton.StartClient();
+    }
+
+    private void HandleClientConnected(ulong clientId)
+    {
+        gameObject.SetActive(false);
     }
 }
